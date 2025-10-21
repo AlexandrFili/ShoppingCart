9125f072cb23612c79cb1044c072dfba059231ca
Филимонов <fili-02@mail.ru>
2025-10-21 21:18:19 +0300

    Добавлен фронт страницы всех товаров и отдельного товара

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -23,5 +23,17 @@ public class HomeController {
 	{
 		return "register";
 	}
+	
+	@GetMapping("/products")
+	public String products() 
+	{
+		return "product";
+	}
+	
+	@GetMapping("/product")
+	public String product() 
+	{
+		return "view_product";
+	}
 
 }
