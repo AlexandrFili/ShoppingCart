4e7308b4e3c4ca3f579d43e343dc0f6d8b530600
Филимонов <fili-02@mail.ru>
2025-10-22 10:57:11 +0300

    Страница добавление товара и добавление категории

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -7,10 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "admin/index";
+	}
+
+	@GetMapping("/loadAddProduct")
+	public String loadAddProduct() {
+		return "admin/add_product";
+	}
+
+	@GetMapping("/category")
+	public String category() {
+		return "admin/category";
 	}
 
 }
