8b957f276d1ef426d531c14dd94fb71eb8a4743a
Филимонов <fili-02@mail.ru>
2025-11-01 19:06:08 +0300

    Создал добавление категории, подключил бд, работает

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,12 +1,34 @@
 package com.ecom.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+	@Autowired
+	private CategoryService categoryService;
 
 	@GetMapping("/")
 	public String index() {
@@ -21,6 +43,37 @@ public class AdminController {
 	@GetMapping("/category")
 	public String category() {
 		return "admin/category";
+	}
+
+	@PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file, HttpSession session) throws IOException {
+
+		String imageName = file != null ? file.getOriginalFilename(): "default.jpg";	
+		category.setImageName(imageName);
+		
+		Boolean existsCategory = categoryService.existCategory(category.getName());
+
+		if (existsCategory) {
+			session.setAttribute("errorMsg", "Такое название категории уже существует!");
+		} else {
+
+			Category saveCategory = categoryService.saveCategory(category);
+			if (ObjectUtils.isEmpty(saveCategory)) {
+				session.setAttribute("errorMsg", "Не сохранилось! Внутренняя ошибка сервера");
+			} else {
+				
+				File saveFile = new ClassPathResource("static/img").getFile();
+				
+				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+"category_img"+File.separator+file.getOriginalFilename());
+				
+				System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				
+				session.setAttribute("succMsg", "Категории успешно сохранились");
+			}
+		}
+
+		return "redirect:/admin/category";
 	}
 
 }
