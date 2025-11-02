cec8cc4920186cdceb9179365d918f5a8327acbd
Филимонов <fili-02@mail.ru>
2025-11-02 15:11:51 +0300

    Добавил функцию просмотра,удаления и редактирования категорий

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -10,9 +10,11 @@ import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,16 +43,18 @@ public class AdminController {
 	}
 
 	@GetMapping("/category")
-	public String category() {
+	public String category(Model m) {
+		m.addAttribute("categorys", categoryService.getAllCategory());
 		return "admin/category";
 	}
 
 	@PostMapping("/saveCategory")
-	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file, HttpSession session) throws IOException {
+	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
+			HttpSession session) throws IOException {
 
-		String imageName = file != null ? file.getOriginalFilename(): "default.jpg";	
+		String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
 		category.setImageName(imageName);
-		
+
 		Boolean existsCategory = categoryService.existCategory(category.getName());
 
 		if (existsCategory) {
@@ -61,19 +65,79 @@ public class AdminController {
 			if (ObjectUtils.isEmpty(saveCategory)) {
 				session.setAttribute("errorMsg", "Не сохранилось! Внутренняя ошибка сервера");
 			} else {
-				
+
 				File saveFile = new ClassPathResource("static/img").getFile();
-				
-				Path path = Paths.get(saveFile.getAbsolutePath()+File.separator+"category_img"+File.separator+file.getOriginalFilename());
-				
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
+						+ file.getOriginalFilename());
+
 				System.out.println(path);
 				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-				
+
 				session.setAttribute("succMsg", "Категории успешно сохранились");
 			}
 		}
 
 		return "redirect:/admin/category";
+	}
+
+	@GetMapping("/deleteCategory/{id}")
+	public String deleteCategory(@PathVariable int id, HttpSession session) {
+		Boolean deleteCategory = categoryService.deleteCategory(id);
+
+		if (deleteCategory) {
+			session.setAttribute("succMsg", "Категория была удалена!");
+		} else {
+			session.setAttribute("errorMsg", "Что-то не так на сервере.");
+
+		}
+
+		return "redirect:/admin/category";
+	}
+
+	@GetMapping("/loadEditCategory/{id}")
+	public String loadEditCategory(@PathVariable int id, Model m) {
+
+		m.addAttribute("category", categoryService.getCategoryById(id));
+		return "admin/edit_category";
+	}
+
+	@PostMapping("/updateCategory")
+	public String updateCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
+			HttpSession session) throws IOException {
+
+		Category oldCategory = categoryService.getCategoryById(category.getId());
+		String imageName = file.isEmpty() ? oldCategory.getImageName() : file.getOriginalFilename();
+
+		if (!ObjectUtils.isEmpty(category)) {
+
+			oldCategory.setName(category.getName());
+			oldCategory.setIsActive(category.getIsActive());
+			oldCategory.setImageName(imageName);
+
+		}
+
+		Category updateCategory = categoryService.saveCategory(oldCategory);
+
+		if (!ObjectUtils.isEmpty(updateCategory)) {
+
+			if (!file.isEmpty()) {
+
+				File saveFile = new ClassPathResource("static/img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
+						+ file.getOriginalFilename());
+
+				//System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			}
+
+			session.setAttribute("succMsg", "Категория была обновлена");
+		} else {
+			session.setAttribute("errorMsg", "Что-то не так на сервере.");
+		}
+
+		return "redirect:/admin/loadEditCategory/" + category.getId();
 	}
 
 }
--- src/main/java/com/ecom/service/CommonServiceImpl.java
+++ src/main/java/com/ecom/service/CommonServiceImpl.java
@@ -20,4 +20,6 @@ public class CommonServiceImpl implements CommonService {
 
 	}
 
+	
+	
 }
