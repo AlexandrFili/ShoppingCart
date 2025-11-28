2569d4cb6515d7fa4ace2fee9004c03e7cf3f8e2
Филимонов <fili-02@mail.ru>
2025-11-28 12:53:04 +0300

    Добавил страницу "Мои заказы" и действие по отмене заказа

--- src/main/java/com/ecom/controller/UserController.java
+++ src/main/java/com/ecom/controller/UserController.java
@@ -16,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.ecom.model.Cart;
 import com.ecom.model.Category;
 import com.ecom.model.OrderRequest;
+import com.ecom.model.ProductOrder;
 import com.ecom.model.UserDtls;
 import com.ecom.service.CartService;
 import com.ecom.service.CategoryService;
 import com.ecom.service.OrderService;
 import com.ecom.service.UserService;
+import com.ecom.util.OrderStatus;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -134,6 +136,38 @@ public class UserController {
 		
 		return "/user/success";
 
+	}
+	
+	@GetMapping("/user-orders")
+	public String myOrder(Model m, Principal p) {
+		UserDtls loginUser = getLoggedInUserDetails(p);
+		List <ProductOrder> orders = orderService.getOrdersByUser(loginUser.getId());
+		m.addAttribute("orders", orders);
+		
+		return "/user/my_orders";
+
+	}
+	
+	@GetMapping("/update-status")
+	public String updateOrderStatus(@RequestParam Integer id, @RequestParam Integer st, HttpSession session) {
+
+		OrderStatus[] values = OrderStatus.values();
+		String status = null;
+
+		for (OrderStatus orderSt : values) {
+			if (orderSt.getId().equals(st)) {
+				status = orderSt.getName();
+			}
+		}
+
+		Boolean updateOrder = orderService.updateOrderStatus(id, status);
+
+		if (updateOrder) {
+			session.setAttribute("succMsg", "Статус обновился!");
+		} else {
+			session.setAttribute("errorMsg", "Статус не обновился!");
+		}
+		return "redirect:/user/user-orders";
 	}
 	
 }
